bce743d2a49c7f31998d22a0687c42a568a456c2
xinrui.an <xinrui.an@lkcoffee.com>
2025-04-18 13:18:09 +0800

    add state example

--- tut_11/State.hs
+++ tut_11/State.hs
@@ -29,6 +29,31 @@ get = St $ \m -> (m, m)
 set :: Mem -> State Mem ()
 set x = St $ \m -> ((), x)
 
+-- 新建一个内存槽 --- State Monad 返回的是最终的状态
+-- mem :: [Int] 
+newMem :: State Mem Int 
+newMem = do 
+  mem <- get 
+  let addr = length mem 
+  set (mem ++ [0])
+  return addr 
+
+-- 获取该地址所存储的变量的值
+deref :: Int -> State Mem Int 
+deref i = do 
+  mem <- get 
+  return $ mem !! i 
+
+-- 赋值
+assign :: Int -> Int -> State Mem Int 
+assign i v = do 
+  mem <- get 
+  let mem' = take i mem ++ [v] ++ drop (i + 1) mem 
+  set mem'
+  return v 
+
+main :: IO()
+main = runTest
 -------------------------- TEST ---------------------------
 
 doubleTick :: Int -> State Int Int 
@@ -46,17 +71,34 @@ doubleincState = fmap (* 2) incrementState
 incrementState :: State Int Int
 incrementState = St $ \s -> (s + 2, s + 1)
 
-main :: IO()
-main = do 
-  let (r1, r2) = runState incrementState 4
-  print r1 
-  print r2
-  let (r1, r2) = runState doubleincState 4
-  print r1 
-  print r2
-  let (r1, r2) = runState pureState 4
-  print r1 
-  print r2
-  let (r1, r2) = runState monadState 4
-  print r1 
-  print r2
+runTest :: IO()
+runTest = do 
+  let (r1a, r2a) = runState incrementState 4
+  print r1a 
+  print r2a
+
+  let (r1b, r2b) = runState doubleincState 4
+  print r1b 
+  print r2b
+
+  let (r1c, r2c) = runState pureState 4
+  print r1c 
+  print r2c
+
+  let (r1d, r2d) = runState monadState 4
+  print r1d 
+  print r2d
+
+  putStrLn "==============State Monad================="
+
+  let (i1, e1) = runState newMem [1..4]
+  print i1 
+  print e1
+
+  let (i2, e2) = runState (assign i1 99) e1
+  print i2 
+  print e2 
+
+  let (i3, e3) = runState (deref i1) e2
+  print i3 
+  print e3  
